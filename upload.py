6778f9da8b33381aaa4f2dbd704c44864a5c1157
Alex Forsythe <awforsythe@gmail.com>
2023-09-10 23:47:51 -0400

    Rotate images to vertical before upload; store and set S3 metadata

--- upload.py
+++ upload.py
@@ -1,11 +1,15 @@
 import os
 import re
+import io
 import sys
 import hashlib
+import json
+from dataclasses import dataclass, asdict
 
 from dotenv import load_dotenv
 import cv2
 import numpy as np
+from colorthief import ColorThief
 import boto3
 import botocore
 
@@ -21,7 +25,39 @@ POST_SCAN_BLACK_POINT = 16
 POST_SCAN_WHITE_POINT = 244
 POST_SCAN_SATURATION_SCALE = 1.15
 
-FULL_RECOPY = len(sys.argv) > 1 and sys.argv[1] == '--full-recopy'
+FULL_RECOPY = len(sys.argv) > 1 and '--full-recopy' in sys.argv
+FULL_REUPLOAD = len(sys.argv) > 1 and '--full-reupload' in sys.argv
+
+
+@dataclass
+class ImageMetadata:
+    width: int
+    height: int
+    color: str
+    rotated: bool
+
+    def to_json_string_dict(self):
+        return {
+            "width": str(self.width),
+            "height": str(self.height),
+            "color": self.color,
+            "rotated": "true" if self.rotated else "false",
+        }
+
+    def save(self, filepath):
+        with open(filepath, 'w') as fp:
+            json.dump(asdict(self), fp)
+    
+    @classmethod
+    def load(cls, filepath):
+        with open(filepath) as fp:
+            data = json.load(fp)
+        return cls(
+            width=data['width'],
+            height=data['height'],
+            color=data['color'],
+            rotated=data['rotated'],
+        )
 
 
 def require_env_var(name):
@@ -59,6 +95,8 @@ def get_local_hashes(dirpath):
     hashes = {}
     if os.path.isdir(dirpath):
         for filename in os.listdir(dirpath):
+            if os.path.splitext(filename)[1] == '.json':
+                continue
             with open(os.path.join(dirpath, filename), 'rb') as fp:
                 h = hashlib.md5()
                 while chunk := fp.read(8192):
@@ -108,6 +146,14 @@ def saturate(image, saturation_scale):
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR).astype(np.uint8)
 
 
+def get_dominant_color(data):
+    with io.BytesIO(data) as buf:
+        thief = ColorThief(buf)
+        rgb = thief.get_color(quality=1)
+    r, g, b = rgb
+    return '#%02x%02x%02x' % (r, g, b)
+
+
 def copy_to_storage():
     if not os.path.isdir(STORAGE_DIR):
         os.makedirs(STORAGE_DIR)
@@ -121,11 +167,36 @@ def copy_to_storage():
         storage_filepath = os.path.join(STORAGE_DIR, basename + '.jpg')
 
         print('Compressing image %s and copying to storage...' % basename)
-        image = cv2.imread(scan_filepath)
-        image = saturate(adjust_levels(image, POST_SCAN_BLACK_POINT, POST_SCAN_WHITE_POINT), POST_SCAN_SATURATION_SCALE)
-        image = cv2.resize(image, [int(image.shape[1] * FULL_IMAGE_SCALE), int(image.shape[0] * FULL_IMAGE_SCALE)], interpolation=cv2.INTER_AREA)
-        cv2.imwrite(storage_filepath, image, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
 
+        # Read the original image and check its dimensions
+        image = cv2.imread(scan_filepath)
+        orig_h, orig_w = image.shape[:2]
+        needs_rotate = orig_w > orig_h
+
+        # Apply some image adjustments and ensure that the image is oriented vertically
+        image = saturate(adjust_levels(image, POST_SCAN_BLACK_POINT, POST_SCAN_WHITE_POINT), POST_SCAN_SATURATION_SCALE)
+        if needs_rotate:
+            image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+        # Scale the image down for the final compressed version
+        new_h, new_w = [int(image.shape[1] * FULL_IMAGE_SCALE), int(image.shape[0] * FULL_IMAGE_SCALE)]
+        resized = cv2.resize(image, [new_h, new_w], interpolation=cv2.INTER_AREA)
+        cv2.imwrite(storage_filepath, resized, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+
+        # Find the dominant color in this image
+        ok, png_bytes = cv2.imencode(".png", resized)
+        color = get_dominant_color(png_bytes)
+
+        # Write a .json file alongside the image containing metadata for the S3 object
+        metadata = ImageMetadata(
+            width=new_w,
+            height=new_h,
+            color=color,
+            rotated=needs_rotate,
+        )
+        metadata.save(storage_filepath + '.json')
+
+        # For an 'a' image, working from the pre-resize image, generate a thumbnail
         if basename.endswith('_a'):
             print('Generating thumbnail for image %s...' % basename)
             thumbnail_filepath = os.path.join(STORAGE_DIR, basename.replace('_a', '') + '_thumb.jpg')
@@ -142,7 +213,10 @@ def sync_to_remote(s3, bucket_name):
     for filename, local_hash in sorted(local_hashes.items()):
         needs_upload = False
         remote_hash = remote_hashes.get(filename, '')
-        if not remote_hash:
+        if FULL_REUPLOAD:
+            print('+ %s will be uploaded.' % filename)
+            needs_upload = True
+        elif not remote_hash:
             print('+ %s is new.' % filename)
             needs_upload = True
         elif remote_hash != local_hash:
@@ -153,14 +227,20 @@ def sync_to_remote(s3, bucket_name):
 
         if needs_upload:
             data = b''
-            with open(os.path.join(STORAGE_DIR, filename), 'rb') as fp:
+            image_filepath = os.path.join(STORAGE_DIR, filename)
+            with open(image_filepath, 'rb') as fp:
                 data = fp.read()
+
+            metadata = {}
+            metadata_filepath = image_filepath + '.json'
+            if os.path.isfile(metadata_filepath):
+                metadata = ImageMetadata.load(metadata_filepath).to_json_string_dict()
 
             sys.stdout.write('Uploading %s (%d bytes)... ' % (filename, len(data)))
             try:
                 assert os.path.splitext(filename)[1].lower() in ('.jpg', '.jpeg')
                 content_type = 'image/jpeg'
-                s3.put_object(Bucket=bucket_name, Key=filename, Body=data, ACL='public-read', ContentType=content_type)
+                s3.put_object(Bucket=bucket_name, Key=filename, Body=data, ACL='public-read', ContentType=content_type, Metadata=metadata)
             except:
                 print('')
                 raise
